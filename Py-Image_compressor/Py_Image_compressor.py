5ca21ee7825066f83c923a8be98918a6915d0e11
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-03 08:15:18 +0000

    Project completed * all functions completed * features added

--- Py-Image_compressor/Py_Image_compressor.py
+++ Py-Image_compressor/Py_Image_compressor.py
@@ -4,10 +4,12 @@ from PIL import Image
 from tkinter.filedialog import *
 import os
 
+# create window/root GUI
 window = Tk()
 
 
 def load_img():
+    """This function will allow the user to select an image from their system's file"""
     global img
     img_path = askopenfilename()
     img = PIL.Image.open(img_path)
@@ -25,12 +27,13 @@ def load_img():
     # green colour for user to know that an image is selected
     if len(img_path) > 1:
         path_name.config(text=img_path, fg="green")
+        save_Btn.config(state="active")
 
 
 def save_img():
     """This function is used to select a directory on your system to save the compressed image"""
     save_path = asksaveasfilename()
-    img.save(save_path)
+    img.save(save_path + ".JPG")
 
 
 # title
@@ -47,10 +50,14 @@ select_Btn.place(x=90, y=100)
 
 save_Btn = Button(window, text="Save Image", width=22, height=2, command=save_img)
 save_Btn.place(x=290, y=100)
+save_Btn.config(state="disabled")
 
 # Window configurations
 window.title("Arsalan's Image Compressor")
-window.geometry("550x250")
+# GUI size
+window.geometry("550x200")
+# stops user changing the GUI size
 window.resizable(0, 0)
+# will always stay on top of all applications
 window.attributes("-topmost", True)
 window.mainloop()
